6b1ac3ca8b585c52bc6b517d14f31c3609dbb6d7
Hayley Song <haejinso@usc.edu>
2019-12-10 19:03:42 -0800

    adds stamen, nls, osm map tile server base urls

---
+++ maptile_urls.py
@@ -0,0 +1,8 @@
+urlbase_osm = "https://b.tile.openstreetmap.org/%d/%d/%d.png"
+urlbase_nls = "https://nls-1.tileserver.com/5eF1a699E49r/%d/%d/%d.jpg"
+
+def get_stamen_url(style:str , x:int , y:int , z:int ):
+    suffix = 'jpg'
+    if style == 'toner':
+        suffix = 'png'
+    return f"http://tile.stamen.com/{style}/{z}/{x}/{y}.{suffix}"
