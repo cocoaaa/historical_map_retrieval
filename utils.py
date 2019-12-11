2f00e78c1637d10fd6a6f7ddb40f2536ba36964a
Hayley Song <haejinso@usc.edu>
2019-12-10 19:03:17 -0800

    adds basic utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+# utils.py
+
+from pathlib import Path
+from typing import Union, Callable
+
+def makedir(dirpath: Union[str, Path], parents=True, exist_ok=True, verbose=True) -> Path:
+    if not isinstance(dirpath, Path):
+        dirpath = Path(dirpath)
+    if dirpath.exists():
+        return dirpath
+    else:
+        dirpath.mkdir(parents=parents, exist_ok=exist_ok)
+        if verbose:
+            print('Created: ', dirpath)
+        return dirpath
+
