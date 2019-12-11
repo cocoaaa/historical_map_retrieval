27d53d3c10f1ad1b263b7145b510bf7be25c8a9f
Hayley Song <haejinso@usc.edu>
2019-12-11 15:35:11 -0800

    adds download functions for multiple map-tiles

--- downloader.py
+++ downloader.py
@@ -80,6 +80,7 @@ def store_4Geo_Boundary(lnglat_path: str, x, y, z):
 
 
 def getImgFromUrl(out_dir: Union[str, Path], url: str, x, y, z):
+    checkBlankImg_fn = checkBlankImg_nls if 'nls' in url else checkBlankImg_ggl
     req = ur.Request(url)
     try:
         urlobj = ur.urlopen(req)
@@ -110,7 +111,7 @@ def getImgFromUrl(out_dir: Union[str, Path], url: str, x, y, z):
             return
 
         # Check if the image is blank, i.e: sea, plain grass
-        if checkBlankImg_ggl(out_fn):  # or checkBlankImg_ggl(out_fn):
+        if checkBlankImg_fn(out_fn):  # or checkBlankImg_ggl(out_fn):
             if out_fn.exists():
                 out_fn.unlink()
                 print("Deleted: ", x, y, z)
@@ -140,7 +141,6 @@ def downlod_tiles_by_xyz(out_dir: Union[str, Path], url_base: str,
             getImgFromUrl(out_dir, url_tile, x, y, z)
             time.sleep(0.005)
 
-
 def download_tiles_by_lnglat(out_dir: Union[str, Path], url_base: str,
                              start_long, end_long, start_lat, end_lat, zoom):
     x0, y0, z = getTileFromGeo(start_lat, start_long, zoom)
@@ -156,24 +156,16 @@ def download_tiles_by_lnglat(out_dir: Union[str, Path], url_base: str,
     print('Downloading...', start_x, end_x, start_y, end_y)
     downlod_tiles_by_xyz(out_dir, url_base, start_x, end_x, start_y, end_y, z)
 
-
-def download_stamen_styles(locations_fn: str, styles: Iterable[str], out_dir_root: Union[str,Path]):
-    """
-	styles = ['toner', 'terrain', 'watercolor']
-
-	:param locations_fn: path to the json file with city_name:bbox_dictionary
-	:param styles:
-	:param out_dir_root:
-	:return:
-	"""
-
+def download_tiles_from_cities(locations_fn: str, tile_source_name:str, styles: Iterable[str], out_dir_root: Union[str,Path]):
     out_dir_root = makedir(out_dir_root)
+
     with open(locations_fn) as f:
         city_geos = json.load(f)
     print(list(city_geos.keys()))
+
     for city, geo in tqdm.tqdm(city_geos.items(), desc='city-loop'):
-        if city in ['paris', 'khartoum']:
-            continue
+        # if city in ['paris', 'khartoum']:
+        #     continue
         xmin, xmax, ymin, ymax = geo['xmin'], geo['xmax'], geo['ymin'], geo['ymax']
         z = geo.get('z', 15)
 
@@ -184,7 +176,7 @@ def download_stamen_styles(locations_fn: str, styles: Iterable[str], out_dir_roo
             out_dir = makedir(out_dir)
 
             print('Started style: ', city, style)
-            ts_name = f'Stamen{snake2camel(style)}'
+            ts_name = f'{tile_source_name}{snake2camel(style)}'
             url_base = ts.tile_sources[ts_name]
             print(f'style: {ts_name}, \nurl_base: {url_base}')
             download_tiles_by_lnglat(out_dir, url_base, xmin, xmax, ymin, ymax, z)
@@ -192,12 +184,74 @@ def download_stamen_styles(locations_fn: str, styles: Iterable[str], out_dir_roo
         print(f'Done {city}\n\n')
 
 
+def download_stamen_styles(locations_fn: str, styles: Iterable[str], out_dir_root: Union[str,Path]):
+    """
+	styles = ['toner', 'toner_background', 'terrain', 'watercolor']
+
+	:param locations_fn: path to the json file with city_name:bbox_dictionary
+	:param styles:
+	:param out_dir_root:
+	:return:
+	"""
+    for style in styles:
+        assert style.lower() in ['toner', 'toner_background', 'terrain', 'watercolor'], f'{style} is not a valid style name'
+    tile_source_name = 'Stamen'
+    download_tiles_from_cities(locations_fn, tile_source_name, styles, out_dir_root)
+
+
+def download_esri_styles(locations_fn: str, styles: Iterable[str], out_dir_root: Union[str,Path]):
+    for style in styles:
+        assert style.lower() in ['imagery', 'nat_geo', 'terrain', 'reference', 'ocean_base', 'ocean_reference' ], f'{style} is not a valid style name'
+    tile_source_name = 'Esri'
+    download_tiles_from_cities(locations_fn, tile_source_name, styles, out_dir_root)
+
+def download_carto_styles(locations_fn: str, styles: Iterable[str], out_dir_root: Union[str,Path]):
+    for style in styles:
+        assert style.lower() in ['dark', 'light', 'eco', 'midnight'], f'{style} is not a valid style name'
+    tile_source_name = 'Carto'
+    download_tiles_from_cities(locations_fn, tile_source_name, styles, out_dir_root)
+
 def download_osm(locations_fn: str, out_dir_root: str):
-    pass
+    out_dir_root = makedir(out_dir_root)
 
+    with open(locations_fn) as f:
+        city_geos = json.load(f)
+    print(list(city_geos.keys()))
 
-def download_nls(locations_fn: str, out_dir_root: str):
-    pass
+    for city, geo in tqdm.tqdm(city_geos.items(), desc='city-loop'):
+        if city in ['paris', 'khartoum']:
+            continue
+        xmin, xmax, ymin, ymax = geo['xmin'], geo['xmax'], geo['ymin'], geo['ymax']
+        z = geo.get('z', 15)
+
+        print('=' * 80)
+        print('Started ', city)
+        out_dir = Path(out_dir_root) / city
+        out_dir = makedir(out_dir)
+
+        url_base = ts.tile_sources['OSM']
+        download_tiles_by_lnglat(out_dir, url_base, xmin, xmax, ymin, ymax, z)
+        print(f'Done {city}\n\n')
+
+def download_nls(locations_fn: str, out_dir_root: str, z=16):
+    out_dir_root = makedir(out_dir_root)
+
+    with open(locations_fn) as f:
+        city_geos = json.load(f)
+    print(list(city_geos.keys()))
+
+    for city, geo in tqdm.tqdm(city_geos.items(), desc='city-loop'):
+        xmin, xmax, ymin, ymax = geo['xmin'], geo['xmax'], geo['ymin'], geo['ymax']
+        z = geo.get('z', z)
+
+        print('=' * 80)
+        print('Started ', city)
+        out_dir = Path(out_dir_root) / city
+        out_dir = makedir(out_dir)
+
+        url_base = ts.tile_sources['NLS']
+        download_tiles_by_lnglat(out_dir, url_base, xmin, xmax, ymin, ymax, z)
+        print(f'Done {city}\n\n')
 
 
 if __name__ == "__main__":
@@ -206,7 +260,7 @@ if __name__ == "__main__":
     parser.add_argument("bbox_json", type=str,
                         help="Path to a json file with cityname:bbox in lat,lng")
     parser.add_argument("-ts", "--tile-server", required=True, type=str,
-                        help="name of the tile server. Options: stamen, os, nls")
+                        help="name of the tile server. Options: stamen, esri, carto, osm, nls")
     parser.add_argument("-o", "--out", help="Path to the output root folder. Default: ./tmp",
                         type=str, default='./tmp')
 
@@ -219,5 +273,22 @@ if __name__ == "__main__":
     if tile_server == 'stamen':
         styles = ['toner_background', 'terrain', 'watercolor']
         download_stamen_styles(bbox_json, styles, out_dir)
+
+    elif tile_server == 'esri':
+        styles = ['imagery']#, 'nat_geo', 'terrain']
+        download_esri_styles(bbox_json, styles, out_dir)
+
+    elif tile_server == 'carto':
+        styles = ['dark', 'light']
+        download_carto_styles(bbox_json, styles, out_dir)
+
+    elif tile_server == 'osm':
+        download_osm(bbox_json, out_dir)
+
+    elif tile_server == 'nls':
+        download_nls(bbox_json, out_dir)
+
+
+
 
 # xmin, xmax, ymin, ymax = 52.0100, 52.0500, -1.0000, -0.9500
