5a7a1d4aefec480c9e3810388e41ed251bcd54b4
Hayley Song <haejinso@usc.edu>
2019-12-11 15:35:31 -0800

    minor update

--- maptile_urls.py
+++ maptile_urls.py
@@ -1,6 +1,7 @@
 urlbase_osm = "https://b.tile.openstreetmap.org/%d/%d/%d.png"
 urlbase_nls = "https://nls-1.tileserver.com/5eF1a699E49r/%d/%d/%d.jpg"
 
+# http://nls-3.tileserver.com/nls/{z}/{x}/{y}.jpg
 def get_stamen_url(style:str , x:int , y:int , z:int ):
     suffix = 'jpg'
     if style == 'toner':
